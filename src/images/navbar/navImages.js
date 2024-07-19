07bd161bf3322d8b9254d7d9b847677a111316d4
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-07-19 17:38:11 +0900

    KL-16/remove: remove icon images
    - 아이콘 이미지 삭제

--- src/images/navbar/navImages.js
+++ src/images/navbar/navImages.js
@@ -1,7 +1,3 @@
-import imgFind from './find.png'
-import imgLike from './like.png'
-import imgNoti from './noti.png'
-import imgShow from './show.png'
 import imgLogo from './logo.png'
 
 export default imgLogo
