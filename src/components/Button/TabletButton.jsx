e3b6a85067384512f65a98e79d42bcbaf62e4a5b
seoulyego <youngsk786@gmail.com>
2024-07-19 16:41:08 +0900

    KL-61/rename: modify icons directory
    icons 디렉토리명을 Button으로 변경하였습니다.

---
+++ src/components/Button/TabletButton.jsx
@@ -0,0 +1,59 @@
+import React, { useCallback, useState } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { IoIosArrowDown as ArrowDown } from 'react-icons/io'
+
+const StyledToggle = styled.div`
+  display: flex inline;
+  flex-direction: column;
+  align-items: flex-end;
+  :last-child {
+    margin-top: 0.15rem;
+  }
+`
+
+const StyledTabletButton = styled.div`
+  padding: 0.3rem 0.35rem 0.25rem 0.35rem;
+  border: 0.08rem solid black;
+  border-radius: 1rem;
+  font-size: 0.8rem;
+  display: flex inline;
+  justify-content: space-between;
+  align-items: center;
+  :nth-child(n) {
+    margin-top: 0.05358rem;
+    margin-right: 0.2rem;
+  }
+  :last-child {
+    margin-top: 0rem;
+    margin-right: 0rem;
+  }
+`
+
+function TabletButton({ text, size, ChildComponent }) {
+  const [dropDown, setDropDown] = useState(false)
+  const handleToggle = useCallback(() => {
+    setDropDown((current) => !current)
+  }, [])
+
+  return (
+    <StyledToggle>
+      <StyledTabletButton>
+        <span>{text}</span>
+        <ArrowDown
+          size={size}
+          onClick={handleToggle}
+        />
+      </StyledTabletButton>
+      {dropDown ? <ChildComponent /> : null}
+    </StyledToggle>
+  )
+}
+
+TabletButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  ChildComponent: PropTypes.elementType,
+}
+
+export default TabletButton
