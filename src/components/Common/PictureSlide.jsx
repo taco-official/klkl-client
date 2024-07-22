40062e846dab1e9582349689c491b483980713f0
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-07-22 16:54:56 +0900

    KL-65/design: change picture slide style
    - 버튼, 순서 아이콘 스타일 변경
    - 좀더 이뿌게 ><

--- src/components/Common/PictureSlide.jsx
+++ src/components/Common/PictureSlide.jsx
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa6'
 
 export default function PictureSlide({ images, $width, $height }) {
   const [index, changeIndex] = useState(0)
@@ -35,7 +36,7 @@ export default function PictureSlide({ images, $width, $height }) {
         }}
         $left
       >
-        ◀
+        <FaChevronLeft style={{ width: '100%', height: '100%' }} />
       </ChangeButton>
       {images.map((url) => {
         return <SlideImageContent src={url} />
@@ -45,7 +46,7 @@ export default function PictureSlide({ images, $width, $height }) {
           changeIndex(index + 1 >= images.length ? index : index + 1)
         }}
       >
-        ▶
+        <FaChevronRight style={{ width: '100%', height: '100%' }} />
       </ChangeButton>
       <DotList />
     </PictureSlideWrapper>
@@ -63,13 +64,21 @@ const PictureSlideWrapper = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  &:hover {
+    div {
+      opacity: 1;
+      transition: all ease-in-out 0.5s;
+    }
+  }
 `
 
 const SlideImageContent = styled.img.attrs({
   alt: '',
   className: 'imgslide--content__img',
 })`
-  height: 100%;
   width: 100%;
   opacity: 0%;
 
@@ -80,8 +89,12 @@ const SlideImageContent = styled.img.attrs({
 `
 
 const ChangeButton = styled.div`
-  width: 20px;
-  height: 100%;
+  width: 3%;
+  aspect-ratio: 1 / 1;
+  opacity: 0;
+  border-radius: 50%;
+
+  padding: 8px;
 
   display: flex;
   align-items: center;
@@ -89,19 +102,15 @@ const ChangeButton = styled.div`
 
   position: absolute;
   z-index: 5;
-
   user-select: none;
+
+  color: white;
+  background-color: rgba(255, 255, 255, 0.15);
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
 
   cursor: pointer;
 
-  ${(props) => (props.$left ? `left: 0px` : `right: 0px`)};
-
-  opacity: 0;
-
-  &:hover {
-    opacity: 0.5;
-    transition: 0.5s;
-  }
+  ${(props) => (props.$left ? `left: 5px` : `right: 5px`)};
 `
 
 const DotList = styled.div.attrs({
@@ -113,18 +122,21 @@ const DotList = styled.div.attrs({
   position: absolute;
   bottom: 10px;
 
-  opacity: 30%;
+  opacity: 0;
 
   div {
     z-index: 6;
-    width: 7px;
-    height: 7px;
-    border: 1.5px solid black;
+
+    height: 8px;
+    aspect-ratio: 1 / 1;
+
     border-radius: 50%;
     margin-left: 3px;
+    background-color: lightgrey;
+    box-shadow: 1px 1px 1px grey;
   }
 
   div.image--dot__curr {
-    background-color: black;
+    background-color: white;
   }
 `
