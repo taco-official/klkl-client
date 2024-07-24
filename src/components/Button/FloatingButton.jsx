d1720bb53873ca848eb78c7fc27a5563dfab2b11
seoulyego <youngsk786@gmail.com>
2024-07-25 05:25:11 +0900

    KL-104/refactor: apply IconContext.Provider
    FloatingButton을 감싸는 IconContext.Provider 적용

--- src/components/Button/FloatingButton.jsx
+++ src/components/Button/FloatingButton.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import propTypes from 'prop-types'
-import { FaRegPenToSquare as Icon } from 'react-icons/fa6'
+import { IconContext } from 'react-icons'
+import { FaRegPenToSquare as WriteIcon } from 'react-icons/fa6'
+import theme from '../../style/theme'
 
-const StyledButton = styled.button`
+const StyledButton = styled.div`
   position: fixed;
-  background-color: #2fa7ff;
-  border: none;
   border-radius: 20%;
   padding: 0.6rem 0.6rem 0.8rem 0.8rem;
+  background-color: ${theme.color.main};
+  z-index: 100;
+  box-shadow: 0.13rem 0.13rem 0.22rem 0.06rem gray;
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 100;
-  box-shadow: 0.13rem 0.13rem 0.22rem 0.06rem gray;
 `
 
-function FloatingButton({ size = '2rem' }) {
+function FloatingButton({ iconSize = '2rem' }) {
+  const iconValue = useMemo(
+    () => ({ color: 'white', size: `${iconSize}` }),
+    [iconSize]
+  )
   return (
-    <StyledButton>
-      <Icon
-        size={size}
-        color="white"
-      />
-    </StyledButton>
+    <IconContext.Provider value={iconValue}>
+      <StyledButton>
+        <WriteIcon />
+      </StyledButton>
+    </IconContext.Provider>
   )
 }
 
 FloatingButton.propTypes = {
-  size: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  iconSize: propTypes.oneOfType([propTypes.string, propTypes.number]),
 }
 
 export default FloatingButton
