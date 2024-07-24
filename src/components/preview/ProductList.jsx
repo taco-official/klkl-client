a12b36b4ba88694242ecdb8a6df7b765dff177a7
seoulyego <youngsk786@gmail.com>
2024-07-25 05:28:06 +0900

    KL-104/feat: render LoadingContent when product list is loading
    product list가 로딩 중일 때 LoadingContent 렌더링하도록 추가
    3열 리뷰 미리보기 정렬
    데이터 적용 테스트를 위한 더미 데이터 추가

--- src/components/preview/ProductList.jsx
+++ src/components/preview/ProductList.jsx
@@ -1,52 +1,96 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-// import propTypes from 'prop-types'
-import axios from 'axios'
+// import axios from 'axios'
 import PreviewContent from './PreviewContent'
+import LoadingContent from './LoadingContent'
 
 const StyledList = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-content: flex-start;
-  align-items: flex-start;
-  flex-wrap: wrap;
+  display: inline-grid;
+  grid-template-columns: repeat(3, minmax(auto, 13rem));
 `
 
-function ProductList() {
-  const [loading, setLoading] = useState(true)
-  const [productList, setProductList] = useState([])
-  const [error, setError] = useState(null)
+/* productList 더미 데이터 */
+const user = {
+  id: 54329,
+}
 
+const dummyProductList = [
+  {
+    id: 24456,
+    name: 'Real Sayoon',
+    city: 'Seoul',
+    subcategory: 'Portrait',
+    tags: ['귤락', '10기', '레노버', '42Guest'],
+    likeCount: 100,
+  },
+  {
+    id: 24457,
+    name: 'Oyabung',
+    city: 'Olympic',
+    subcategory: 'Ohhamma',
+    tags: ['오야붕', '함대장', '망치'],
+    likeCount: 101,
+  },
+  {
+    id: 24458,
+    name: 'Yeongo',
+    city: 'Naksungdae',
+    subcategory: 'Fxck-end',
+    tags: ['100m', '패럴림픽'],
+    likeCount: 101,
+  },
+]
+
+function ProductList() {
+  const [productList, setProductList] = useState([])
+  const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState(null)
+
+  // useEffect(() => {
+  // async function getProductList() {
+  // try {
+  // const response = await axios.get('ProductList API')
+  // setProductList(response.data)
+  // setLoading(false)
+  // } catch (e) {
+  // 에러 처리
+  // setError(e)
+  // setLoading(false)
+  // }
+  // }
+  // }, [])
+  // if (error) return <div>Error: {error.message}</div>
+
+  /* 더미데이터 테스트 코드 */
   useEffect(() => {
-    async function getProductList() {
-      try {
-        const response = await axios.get('ProductList API')
-        setProductList(response.data)
+    function getProductList() {
+      setTimeout(() => {
+        setProductList(dummyProductList)
         setLoading(false)
-      } catch (e) {
-        // 에러 처리
-        setError(e)
-        setLoading(false)
-      }
+      }, 3000)
     }
     getProductList()
   }, [])
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error: {error.message}</div>
+
   return (
     <StyledList>
-      {productList.map((content) => (
-        <PreviewContent
-          key={content.name}
-          userId={user.id}
-          productId={content.id}
-          city={content.city}
-          subcategory={content.subcategory}
-          name={content.name}
-          tags={content.tags}
-          likeCount={content.likeCount}
-        />
-      ))}
+      {loading ? (
+        <LoadingContent />
+      ) : (
+        productList.map((content) => (
+          <PreviewContent
+            key={content.name}
+            userId={user.id}
+            productId={content.id}
+            city={content.city}
+            subcategory={content.subcategory}
+            name={content.name}
+            tags={content.tags}
+            likeCount={content.likeCount}
+          />
+        ))
+      )}
+      <LoadingContent />
     </StyledList>
   )
 }
