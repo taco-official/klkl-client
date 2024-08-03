a4af37a2bfb46852c1950b4cb506b4ceb8a9be15
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-04 04:33:30 +0900

    KL-103/design: change comment design
    - change width: 60% -> 100%
    - use antd input
    - add date parsing function

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,126 +1,107 @@
-import { React, useState } from 'react'
+import { React, useState, useRef } from 'react'
+import { Input, Button } from 'antd'
 import styled from 'styled-components'
 import theme from '../../style/theme'
-import StyledButton from '../Common/StyledButton'
 import ProfileImage from '../UserProfile/ProfileImage'
 import CommentList from './CommentList'
 
-const sampleComments = {
-  1: {
+const sampleComments = [
+  {
     user: 'sayoon',
-    image:
-      'https://i.namu.wiki/i/s4A1DnqxgN90LS1t9C4hfB12c6mkMj8J7PoK8EiUfWLPKXBS0TaN0108zVdg0sKLcMIO_bo9_eVPDA32-NHta56Y79aItocYCY5hy8UeeroGjZAYRocc2AmXBxTN9X970DguRlszklFsdYTdxMNB5Q.webp',
+    image: 'https://i.ytimg.com/vi/bOJdHU99OO8/maxresdefault.jpg',
     date: '2024.07.03 01:54',
     content:
       '대통령은 필요하다고 인정할 때에는 외교·국방·통일 기타 국가안위에 관한 중요정책을 국민투표에 붙일 수 있다.',
   },
-  2: {
+  {
     user: 'sanghwal',
-    image:
-      'https://i.namu.wiki/i/s4A1DnqxgN90LS1t9C4hfB12c6mkMj8J7PoK8EiUfWLPKXBS0TaN0108zVdg0sKLcMIO_bo9_eVPDA32-NHta56Y79aItocYCY5hy8UeeroGjZAYRocc2AmXBxTN9X970DguRlszklFsdYTdxMNB5Q.webp',
+    image: 'https://i.ytimg.com/vi/bOJdHU99OO8/maxresdefault.jpg',
     date: '2024.07.03 01:54',
     content:
       '각급 선거관리위원회는 선거인명부의 작성등 선거사무와 국민투표사무에 관하여 관계 행정기관에 필요한 지시를 할 수 있다.',
   },
-}
+]
 
 export default function Comment() {
+  const inputRef = useRef(null)
   const [comments, updateComment] = useState(sampleComments)
 
   const addComment = () => {
-    const inputValue = document.querySelector('input[name="comment"]').value
-    if (inputValue.length === 0) return
-    const newComments = { ...comments }
-    newComments[Object.keys(comments).length + 1] = {
-      user: 'hihi',
-      image:
-        'https://i.namu.wiki/i/s4A1DnqxgN90LS1t9C4hfB12c6mkMj8J7PoK8EiUfWLPKXBS0TaN0108zVdg0sKLcMIO_bo9_eVPDA32-NHta56Y79aItocYCY5hy8UeeroGjZAYRocc2AmXBxTN9X970DguRlszklFsdYTdxMNB5Q.webp',
-      date: 'fuckyou',
-      content: inputValue,
-    }
-    document.querySelector('input[name="comment"]').value = ''
+    const { textArea } = inputRef.current.resizableTextArea
+    const date = new Date()
+    const year = date.getUTCFullYear()
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0') // 월은 0부터 시작하므로 +1 필요
+    const day = String(date.getUTCDate()).padStart(2, '0')
+    const hour = String(date.getUTCHours()).padStart(2, '0')
+    const minute = String(date.getUTCMinutes()).padStart(2, '0')
+
+    if (textArea.value.length === 0) return
+
+    const newComments = [...comments]
+    newComments.push({
+      user: 'anonymous',
+      image: 'https://i.ytimg.com/vi/bOJdHU99OO8/maxresdefault.jpg',
+      date: `${year}.${month}.${day} ${hour}:${minute}`,
+      content: textArea.value,
+    })
+    textArea.value = ''
     updateComment(newComments)
   }
 
   return (
     <CommentBox>
-      <Head>
+      <h2>
         댓글(<span>{Object.keys(comments).length}</span>)
-      </Head>
+      </h2>
       <CommentInputBox>
         <ProfileImage
-          src="https://i.namu.wiki/i/s4A1DnqxgN90LS1t9C4hfB12c6mkMj8J7PoK8EiUfWLPKXBS0TaN0108zVdg0sKLcMIO_bo9_eVPDA32-NHta56Y79aItocYCY5hy8UeeroGjZAYRocc2AmXBxTN9X970DguRlszklFsdYTdxMNB5Q.webp"
-          size="100%"
+          src="https://i.ytimg.com/vi/bOJdHU99OO8/maxresdefault.jpg"
+          $size="100%"
         />
-        <CommentInputForm>
-          <input
-            type="text"
-            name="comment"
-          />
-          <CommentSubmitButton
-            type="button"
-            $state
-            onClick={addComment}
-          >
-            등록
-          </CommentSubmitButton>
-        </CommentInputForm>
+        <Input.TextArea
+          ref={inputRef}
+          rows={1}
+          showCount
+          maxLength={200}
+          size="large"
+          autoSize={{ minRows: 1, maxRows: 3 }}
+          style={{ fontSize: theme.size.textSM }}
+        />
+        <Button onClick={addComment}>등록</Button>
       </CommentInputBox>
       <CommentList comments={comments} />
     </CommentBox>
   )
 }
 
-const Head = styled.h2`
-  font-style: ${theme.style.mainEB};
-  font-size: ${theme.size.titleMD};
-  margin-bottom: 20px;
-  margin-top: 20px;
-`
-
 const CommentBox = styled.div`
-  width: 60%;
+  width: 100%;
   margin: 0 auto;
+
+  h2 {
+    font-style: ${theme.style.mainEB};
+    font-size: ${theme.size.titleSM};
+    margin-bottom: 20px;
+    margin-top: 20px;
+  }
 `
 
 const CommentInputBox = styled.div`
   height: 60px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
-  align-items: flex-end;
-
   margin-bottom: 30px;
-`
 
-const CommentInputForm = styled.form`
-  flex-grow: 0.95;
-  height: 100%;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-
-  padding-bottom: 10px;
-
-  input {
-    height: 70%;
-    flex-grow: 0.9;
+  & > span {
+    width: 80%;
+    margin: 0 15px;
 
     border: none;
-    border-bottom: 1px solid ${theme.color.lineGrey};
-    outline: none;
+    border-bottom: 1px solid lightgray;
 
-    font-family: ${theme.style.main};
+    span {
+      font-size: ${theme.size.text2XS};
+    }
   }
-`
-
-const CommentSubmitButton = styled(StyledButton)`
-  border-radius: 30px;
-  width: 80px;
-  height: 60%;
-  font-family: ${theme.style.mainBold};
-
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
 `
