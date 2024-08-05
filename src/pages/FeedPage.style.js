af42bbc50fec4671947d452eebed2de58c611091
seoulyego <youngsk786@gmail.com>
2024-08-05 15:23:36 +0900

    KL-104/feat: add Feed Page
    Feed Page를 추가하였습니다.
    기본 필터, 추가 필터, 리뷰 미리보기가 포함되어 있습니다.
    
    예정된 작업
    - 선택 필드 추가
    - 페이지 디자인

---
+++ src/pages/FeedPage.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const PageMain = styled.main`
+  display: flex;
+  justify-content: center;
+`
+
+const FeedSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
+export { PageMain, FeedSection }
