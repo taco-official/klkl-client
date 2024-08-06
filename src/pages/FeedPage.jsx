9128ac79778aa21b108d0dd88c4b36bc0a76d42c
seoulyego <youngsk786@gmail.com>
2024-08-06 18:38:18 +0900

    KL-115/refactor: move ContextProvider from each module to the page
    각 모듈에 존재하던 ContextProvider를 FeedPage로 옮겼습니다.
    이제 단일화된 전역 상태 관리가 가능합니다.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { BasicFilterProvider } from '../contexts/BasicFilterContext'
+import { AdditionalFilterProvider } from '../contexts/AdditionalFilterContext'
 // import NavBar from '../components/Navbar/NavBar'
 import BasicFilter from '../components/BasicFilter/BasicFilter'
 import AdditionalFilter from '../components/AdditionalFilter/AdditionalFilter'
+import SelectedField from '../components/SelectedField/SelectedField'
 import ProductList from '../components/preview/ProductList'
 import { PageMain, FeedSection } from './FeedPage.style'
 
 function FeedPage() {
   return (
-    <PageMain>
-      <BasicFilter />
-      <FeedSection>
-        <AdditionalFilter />
-        <ProductList />
-      </FeedSection>
-    </PageMain>
+    <BasicFilterProvider>
+      <PageMain>
+        <BasicFilter />
+        <AdditionalFilterProvider>
+          <FeedSection>
+            <AdditionalFilter />
+            <SelectedField />
+            <ProductList />
+          </FeedSection>
+        </AdditionalFilterProvider>
+      </PageMain>
+    </BasicFilterProvider>
   )
 }
 
