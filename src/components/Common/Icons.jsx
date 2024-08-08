25ff4904f15caadd00bf857de629d9dc0df053af
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-08 18:26:03 +0900

    KL-103/feat: make icon component
    - 구글 웹폰트 편하게 사용할 컴포넌트 제작
    - name prop에 사용할 컴포넌트 이름 전달

---
+++ src/components/Common/Icons.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Icons = styled.span.attrs({
+  className: 'material-symbols-rounded',
+})`
+  font-variation-settings:
+    'FILL' ${({ $empty }) => ($empty ? 0 : 1)},
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 20;
+
+  font-size: ${({ $size }) => $size || '1.2em'};
+`
+
+export default Icons
