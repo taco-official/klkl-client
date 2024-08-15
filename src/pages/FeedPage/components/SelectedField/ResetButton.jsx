5c961c648f434a6d61213cd83911b9477d07a7ba
seoulyego <youngsk786@gmail.com>
2024-08-15 18:57:35 +0900

    KL-143/refactor: apply state store to the ResetButton in SelectedField
    state store를 적용하였습니다.
    불필요한 useCallback을 제거하였습니다.

--- src/pages/FeedPage/components/SelectedField/ResetButton.jsx
+++ src/pages/FeedPage/components/SelectedField/ResetButton.jsx
@@ -1,26 +1,24 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { Button } from 'antd'
 import { GrPowerReset } from 'react-icons/gr'
-import useSelectedCountry from '../../../../hooks/useSelectedCountry'
-import useSelectedCity from '../../../../hooks/useSelectedCity'
-import useSelectedCategory from '../../../../hooks/useSelectedCategory'
-import useSelectedSubCategory from '../../../../hooks/useSelectedSubCategory'
-import useSelectedFilter from '../../../../hooks/useSelectedFilter'
+import useFeedStore from '../../stores/useFeedStore'
 
 function ResetButton() {
-  const { setSelectedCountry } = useSelectedCountry()
-  const { setSelectedCity } = useSelectedCity()
-  const { setSelectedCategory } = useSelectedCategory()
-  const { setSelectedSubCategory } = useSelectedSubCategory()
-  const { setSelectedFilter } = useSelectedFilter()
+  const setSelectedCountry = useFeedStore((state) => state.setSelectedCountry)
+  const setSelectedCity = useFeedStore((state) => state.setSelectedCity)
+  const setSelectedCategory = useFeedStore((state) => state.setSelectedCategory)
+  const setSelectedSubCategory = useFeedStore(
+    (state) => state.setSelectedSubCategory
+  )
+  const setSelectedFilter = useFeedStore((state) => state.setSelectedFilter)
 
-  const handleClick = useCallback(() => {
+  const handleClick = () => {
     setSelectedFilter([])
     setSelectedSubCategory([])
     setSelectedCategory([])
     setSelectedCity([])
     setSelectedCountry({})
-  }, [])
+  }
 
   return (
     <Button
