c384212e6f50881ac673c5bfb3c6a03ab45bfbaa
seoulyego <youngsk786@gmail.com>
2024-08-15 17:02:38 +0900

    KL-143/feat: add state store for FeedPage
    FeedPage를 위한 상태 저장소를 추가하였습니다.

---
+++ src/pages/FeedPage/stores/useFeedStore.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand'
+
+const useFeedStore = create((set) => ({
+  selectedCountry: {},
+  selectedCity: [],
+  selectedCategory: [],
+  selectedSubCategory: [],
+  selectedFilter: [],
+  selectedSort: {},
+  setSelectedCountry: (state) => set({ selectedCountry: state }),
+  setSelectedCity: (state) => set({ selectedCity: state }),
+  setSelectedCategory: (state) => set({ selectedCategory: state }),
+  setSelectedSubCategory: (state) => set({ selectedSubCategory: state }),
+  setSelectedFilter: (state) => set({ selectedFilter: state }),
+  setSelectedSort: (state) => set({ sortBy: state }),
+}))
+
+export default useFeedStore
