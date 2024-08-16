ab6df8368e4c8251eaf11082427ee1d557f10f34
seoulyego <youngsk786@gmail.com>
2024-08-16 13:54:23 +0900

    KL-147/feat: add ConfigProvider
    FeedPage에 ConfigProvider를 추가하였습니다.
    - antd 테마 설정을 변경할 수 있습니다.
    
    StyledComponent의 이름이 일부 변경되었습니다.

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { BasicFilterProvider } from '../../contexts/BasicFilterContext'
-import { AdditionalFilterProvider } from '../../contexts/AdditionalFilterContext'
+import { ConfigProvider } from 'antd'
 // import NavBar from '../components/Navbar/NavBar'
 import Thumbnail from './Thumbnail/Thumbnail'
 import BasicFilter from './BasicFilter/BasicFilter'
@@ -8,25 +7,28 @@ import AdditionalFilter from './AdditionalFilter/AdditionalFilter'
 import SelectedField from './SelectedField/SelectedField'
 import ProductList from './ProductList/ProductList'
 // import Footer from '../../components/Footer/Footer'
-import { PageRoot, PageContent, FeedArea } from './FeedPage.style'
+import {
+  antdTheme,
+  PageContainer,
+  PageContent,
+  FeedArea,
+} from './FeedPage.style'
 
 function FeedPage() {
   return (
-    <PageRoot>
+    <PageContainer>
       <Thumbnail />
-      <BasicFilterProvider>
+      <ConfigProvider theme={antdTheme}>
         <PageContent>
           <BasicFilter />
-          <AdditionalFilterProvider>
-            <FeedArea>
-              <AdditionalFilter />
-              <SelectedField />
-              <ProductList />
-            </FeedArea>
-          </AdditionalFilterProvider>
+          <FeedArea>
+            <AdditionalFilter />
+            <SelectedField />
+            <ProductList />
+          </FeedArea>
         </PageContent>
-      </BasicFilterProvider>
-    </PageRoot>
+      </ConfigProvider>
+    </PageContainer>
   )
 }
 
