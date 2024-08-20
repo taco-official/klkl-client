1b7f121854a85a40975f2f6148015d02b6e91d8a
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-20 16:25:15 +0900

    KL-145/refactor: split into new compoents of buttons
    - 태그 선택하는 버튼을 새로운 컴포넌트로 분리햇습니다.

---
+++ src/pages/SubmitPage/components/CategorySubmit/SelectButtons.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Button, ConfigProvider } from 'antd'
+import theme from '../../../../styles/theme'
+
+const buttonTheme = {
+  token: {
+    fontFamily: theme.style.mainBold,
+    colorPrimary: theme.color.main,
+    colorPrimaryHover: theme.color.mainDark,
+  },
+}
+
+function SelectButtons({ isSelected, enable, disable }) {
+  return (
+    <ConfigProvider theme={buttonTheme}>
+      <Wrapper style={{ marginBottom: '1.875rem' }}>
+        <Button
+          type={isSelected ? 'primary' : 'default'}
+          onClick={enable}
+          style={{ marginRight: '1.5625rem', width: '6.25rem' }}
+        >
+          YES
+        </Button>
+        <Button
+          type={isSelected ? 'default' : 'primary'}
+          onClick={disable}
+          style={{ marginRight: '1.5625rem', width: '6.25rem' }}
+        >
+          NO
+        </Button>
+      </Wrapper>
+    </ConfigProvider>
+  )
+}
+SelectButtons.propTypes = {
+  isSelected: PropTypes.bool.isRequired,
+  enable: PropTypes.func.isRequired,
+  disable: PropTypes.func.isRequired,
+}
+
+const Wrapper = styled.div`
+  width: 37.5rem;
+  display: flex;
+  margin-bottom: 1.875rem;
+`
+
+export default SelectButtons
