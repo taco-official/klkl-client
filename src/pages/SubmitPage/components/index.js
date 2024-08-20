74a4070f8dc8d9e31b8751c513877a6cd20912ce
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-20 16:27:10 +0900

    KL-145/refactor: add index.js, new constants
    - index.js를 추가
    - step magic number 대체 용 상수 추가

---
+++ src/pages/SubmitPage/components/index.js
@@ -0,0 +1,6 @@
+import ImageSubmitForm from './ImageSubmit/ImageSubmitForm'
+import RegionSubmitForm from './RegionSubmit/RegionSubmitForm'
+import InfoSubmitForm from './InfoSubmit/InfoSubmit'
+import CategorSubmitForm from './CategorySubmit/CategorSubmitForm'
+
+export { RegionSubmitForm, ImageSubmitForm, InfoSubmitForm, CategorSubmitForm }
--- src/pages/SubmitPage/constant/submitSteps.js
+++ src/pages/SubmitPage/constant/submitSteps.js
@@ -1,8 +1,9 @@
 const submitSteps = {
-  PICTURE: 0,
+  IMAGE: 0,
   REGION: 1,
   INFO: 2,
   CATEGORY: 3,
+  LOADING: 4,
 }
 
 export default submitSteps
