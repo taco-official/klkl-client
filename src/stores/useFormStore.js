f20eb039a8324032a54aabfb3d2c418cb25dcdf7
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-20 15:26:01 +0900

    KL-145/chore: centralize stores
    - store를 한군데로 모았습니다

---
+++ src/stores/useFormStore.js
@@ -0,0 +1,26 @@
+import { create } from 'zustand'
+
+const initialState = {
+  name: '',
+  description: '',
+  currencyId: 438,
+  price: 0,
+  rating: 0,
+
+  continentId: 0,
+  countryId: 0,
+  cityId: 0,
+  address: '',
+
+  categoryId: 0,
+  subcategoryId: 0,
+  tags: new Set([]),
+}
+
+const useFormStore = create((set) => ({
+  ...initialState,
+  setFormContents: (contents) => set(() => contents),
+  resetFormContents: () => set(() => ({ ...initialState })),
+}))
+
+export default useFormStore
