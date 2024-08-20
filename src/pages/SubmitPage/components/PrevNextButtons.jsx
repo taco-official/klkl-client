6f938003437286f5edd81c86e2df46ede624b443
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-20 16:29:15 +0900

    KL-145/refactor: 다음/이전 단계 버튼 새로운 컴포넌트로 분리

--- src/pages/SubmitPage/components/PrevNextButtons.jsx
+++ src/pages/SubmitPage/components/PrevNextButtons.jsx
@@ -1,61 +1,66 @@
 import React from 'react'
-import { useShallow } from 'zustand/react/shallow'
-import { useNavigate } from 'react-router-dom'
-
+import { Button, ConfigProvider } from 'antd'
+import PropTypes from 'prop-types'
+import theme from '../../../styles/theme'
 import submitSteps from '../constant/submitSteps'
-import StyledButton from '../../../components/Button/StyledButton'
-import useReviewStore from '../stores/useReviewStore'
-import useStepStore from '../stores/useStepStore'
+import useFormStore from '../../../stores/useFormStore'
 
-export default function PrevNextButtons() {
-  const { step, increaseStep, decreaseStep, resetStep } = useStepStore()
-
-  const review = useReviewStore(useShallow((state) => state))
-  const resetReviewContents = useReviewStore(
-    (state) => state.resetReviewContents
-  )
-
-  const navigate = useNavigate()
-
-  const stepValidations = {
-    [submitSteps.PICTURE]: () => true,
-    [submitSteps.REGION]: () =>
-      review.continentId !== 0 && review.countryId !== 0 && review.cityId !== 0,
-    [submitSteps.INFO]: () =>
-      review.name !== '' &&
-      review.description !== '' &&
-      review.price !== 0 &&
-      review.rate !== 0,
-    [submitSteps.CATEGORY]: () =>
-      review.categoryId !== 0 && review.subCategoryId !== 0,
+const checkCanNext = (step, review) => {
+  switch (step) {
+    case submitSteps.IMAGE:
+      return true
+    case submitSteps.REGION:
+      return review.cityId !== 0
+    case submitSteps.INFO:
+      return (
+        review.name !== '' &&
+        review.description !== '' &&
+        review.price !== 0 &&
+        review.rating !== 0
+      )
+    case submitSteps.CATEGORY:
+      return review.subcategoryId !== 0
+    default:
+      return false
   }
+}
+
+export default function PrevNextButtons({ step, goNextStep, goPrevStep }) {
+  const review = useFormStore()
 
   return (
     <div>
-      {step !== submitSteps.PICTURE && (
-        <StyledButton
-          $state={false}
-          onClick={decreaseStep}
-          $width="10.9375rem"
-        >
-          이전
-        </StyledButton>
-      )}
-
-      <StyledButton
-        $state
-        disabled={!stepValidations[step]()}
-        onClick={() => {
-          if (step === submitSteps.CATEGORY) {
-            resetReviewContents()
-            resetStep()
-            navigate('/review')
-          } else increaseStep()
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: theme.style.mainBold,
+            colorPrimary: theme.color.main,
+            colorPrimaryHover: theme.color.mainDark,
+          },
         }}
-        $width="10.9375rem"
       >
-        {step === 3 ? '완료' : '다음'}
-      </StyledButton>
+        {step > submitSteps.IMAGE && (
+          <Button
+            onClick={goPrevStep}
+            style={{ marginRight: '1.5625rem', width: '10.9375rem' }}
+          >
+            이전
+          </Button>
+        )}
+        <Button
+          type="primary"
+          disabled={!checkCanNext(step, review)}
+          onClick={goNextStep}
+          style={{ marginRight: '1.5625rem', width: '10.9375rem' }}
+        >
+          {step === submitSteps.CATEGORY ? '완료' : '다음'}
+        </Button>
+      </ConfigProvider>
     </div>
   )
+}
+PrevNextButtons.propTypes = {
+  step: PropTypes.number.isRequired,
+  goNextStep: PropTypes.func.isRequired,
+  goPrevStep: PropTypes.func.isRequired,
 }
