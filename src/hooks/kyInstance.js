44ab0332fe8153c3afaf29a3c8504dc418595364
seoulyego <youngsk786@gmail.com>
2024-08-27 18:59:06 +0900

    KL-147/rename: separate kyInstance to new file
    kyInstance를 별도 파일로 분리하였습니다.
    useKy의 fetch dependency가 추가하였습니다.

--- src/hooks/kyInstance.js
+++ src/hooks/kyInstance.js
@@ -7,11 +7,29 @@ const method = {
   DELETE: 'delete',
 }
 
+// const printLogUrl = (request) => {
+// console.log('request url', request.url)
+// }
+
+// const printLogResponse = async (request, instance, response) => {
+// console.log('response', await response.json())
+// }
+
 const kyInstance = ky.create({
   prefixUrl: 'http://localhost:8080/v1/',
   timeout: 5000,
   headers: {
     'content-type': 'application/json',
+  },
+  hooks: {
+    // beforeRequest: [printLogUrl],
+    // afterResponse: [printLogResponse],
+    // beforeError: [],
+  },
+  retry: {
+    limit: 0,
+    methods: ['get', 'post', 'put'],
+    statusCodes: [500],
   },
 })
 
