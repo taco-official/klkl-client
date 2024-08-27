09dcbcd9e4eecb8b8dfeadfc2db784cdb72345b1
seoulyego <youngsk786@gmail.com>
2024-08-27 21:47:59 +0900

    KL-147/design: add config provider for antd checkbox
    antd checkbox의 디자인을 위한 config provider를 적용하였습니다.

--- src/pages/FeedPage/components/BasicFilter/BasicFilter.jsx
+++ src/pages/FeedPage/components/BasicFilter/BasicFilter.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { ConfigProvider } from 'antd'
 import CountrySection from './CountrySection'
 import CitySection from './CitySection'
 import CategorySection from './CategorySection'
 import { FilterContainer } from './BasicFilter.style'
+import { fontTheme } from '../../FeedPage.style'
 
 function BasicFilter() {
   return (
     <FilterContainer>
-      <CountrySection className="section" />
-      <CitySection className="section" />
-      <CategorySection className="section" />
+      <ConfigProvider theme={fontTheme}>
+        <CountrySection className="section" />
+        <CitySection className="section" />
+        <CategorySection className="section" />
+      </ConfigProvider>
     </FilterContainer>
   )
 }
