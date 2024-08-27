dac5b4e2c81362490199d88694bfae5b87b1cd65
seoulyego <youngsk786@gmail.com>
2024-08-27 18:59:06 +0900

    KL-147/rename: separate kyInstance to new file
    kyInstance를 별도 파일로 분리하였습니다.
    useKy의 fetch dependency가 추가하였습니다.

---
+++ src/hooks/kyInstance.js
@@ -0,0 +1,38 @@
+import ky from 'ky'
+
+const method = {
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'delete',
+}
+
+// const printLogUrl = (request) => {
+// console.log('request url', request.url)
+// }
+
+// const printLogResponse = async (request, instance, response) => {
+// console.log('response', await response.json())
+// }
+
+const kyInstance = ky.create({
+  prefixUrl: 'http://localhost:8080/v1/',
+  timeout: 5000,
+  headers: {
+    'content-type': 'application/json',
+  },
+  hooks: {
+    // beforeRequest: [printLogUrl],
+    // afterResponse: [printLogResponse],
+    // beforeError: [],
+  },
+  retry: {
+    limit: 0,
+    methods: ['get', 'post', 'put'],
+    statusCodes: [500],
+  },
+})
+
+export default kyInstance
+
+export { method }
--- src/hooks/useKy.js
+++ src/hooks/useKy.js
@@ -1,29 +1,14 @@
 import { useState, useEffect } from 'react'
-import ky from 'ky'
-
-const kyInstance = ky.create({
-  prefixUrl: 'http://localhost:8080/v1/',
-  timeout: 5000,
-  // headers: {},
-  // hooks: {
-  // beforeRequest: [],
-  // afterResponse: [],
-  // beforeError: [],
-  // },
-  retry: {
-    limit: 2,
-    methods: ['get', 'post', 'put'],
-    statusCodes: [400, 500],
-  },
-})
+import kyInstance from './kyInstance'
 
 function useKy(initialConfig = null, autoFetch = false, initialData = null) {
-  const [data, setData] = useState(initialData)
   const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(initialData)
   const [error, setError] = useState(null)
 
   const fetchData = async (config) => {
     setLoading(true)
+    setError(null)
     try {
       const response = await kyInstance(config.url, config).json()
       setData(response)
@@ -36,9 +21,9 @@ function useKy(initialConfig = null, autoFetch = false, initialData = null) {
 
   useEffect(() => {
     if (autoFetch && initialConfig) fetchData(initialConfig)
-  }, [])
+  }, [initialConfig])
 
-  return { data, loading, error, fetchData }
+  return { loading, data, error, fetchData }
 }
 
 export default useKy
