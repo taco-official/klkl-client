d028b0bffa0e17e41674c7d1267e634b6a956b46
seoulyego <youngsk786@gmail.com>
2024-08-27 19:06:10 +0900

    KL-147/feat: parse QueryParameters
    쿼리 파리미터가 포함된 url로 변환합니다.
    
    Prototype: requestQueryArray
    [
    	{
    		key: string,
    		value: Array<number | string | boolean>,
    	},
    ]

--- src/hooks/useKyQuery.js
+++ src/hooks/useKyQuery.js
@@ -1,22 +1,31 @@
-import { useQuery } from '@tanstack/react-query'
-import { kyInstance } from './kyInstance'
+import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query'
+import { kyInstance, method } from './kyInstance'
+import parseQueryParams from '../utils/parseQueryParams'
 
 /**
  * Ky기반 useQeury 호출 훅
  *
- * @param {string} httpMethod http 메서드 (get)
- * @param {string} uri 요청할 uri
+ * @param {string} endPoint 요청할 엔드포인트 URL
+ * @param {Array} requestQuery 요청시 넣을 쿼리 객체 (default: null)
+ *  Type: Array<Object<string, Array<number | string | boolean>>>
  * @param {object} options 요청시 넣을 옵션 (default: null)
  * @return useQuery의 return과 동일
  */
-const useKyQuery = (uri, options = null) =>
-  useQuery({
-    queryKey: [uri],
+const useKyQuery = (
+  endPoint,
+  requestQuery = null,
+  queryKey = null,
+  options = null
+) => {
+  const uri = parseQueryParams(endPoint, requestQuery)
+
+  return useQuery({
+    queryKey: queryKey || [endPoint],
     queryFn: () => kyInstance.get(uri).json(),
-    retry: false,
     cacheTime: 300000,
     staleTime: 300000,
     ...options,
   })
+}
 
 export default useKyQuery
