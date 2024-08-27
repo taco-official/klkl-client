7183fdfd6f588860ca116a652a3327fd92dfb69d
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-08-27 15:07:07 +0900

    KL-159/refactor: remove fetching exchage information

--- src/pages/ReviewPage/ReviewMiddle.jsx
+++ src/pages/ReviewPage/ReviewMiddle.jsx
@@ -1,77 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import ky from 'ky'
-import { useQuery } from '@tanstack/react-query'
 import Icons from '../../components/Icons/Icons'
 import theme from '../../styles/theme'
 
-const useExchangeData = (currency) => {
-  const {
-    data: exchanges,
-    isError,
-    isLoading,
-  } = useQuery({
-    queryKey: ['exchange'],
-    queryFn: () =>
-      ky
-        .get(
-          'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=DsijYpQUe0gJa5Q81gZLgzY5Hfrim8I1&data=AP01'
-        )
-        .json(),
-    retry: false,
-    cacheTime: 86400000,
-    staleTime: 86400000,
-  })
-
-  let exchangeCurrency = {}
-
-  if (!isLoading && !isError) {
-    exchangeCurrency = {
-      ...exchanges.find((data) => data.cur_unit.slice(0, 3) === currency.code),
-    }
-
-    exchangeCurrency.bkpr = exchangeCurrency.bkpr.replace(',', '')
-
-    exchangeCurrency.bkpr =
-      exchangeCurrency.cur_unit.length > 3
-        ? parseInt(exchangeCurrency.bkpr / 100, 10)
-        : Number(exchangeCurrency.bkpr)
-  }
-
-  return { exchangeCurrency, isError, isLoading }
-}
-
 function ReviewMiddleBlock({ address, price, currency }) {
-  const { exchangeCurrency, isLoading, isError } = useExchangeData(currency)
-  const [currencyState, setCurrencyState] = useState(false)
-
-  if (isLoading) return <div>loading</div>
-
   return (
     <Wrapper>
       <LocationBox>
         <Icons>location_on</Icons>
         {address === '' ? '상세 위치가 없습니다' : address}
       </LocationBox>
-      {isError ? (
-        <PriceBox>
-          <img src={currency.flag} />
-          {`${currency.code} ${price}`}
-        </PriceBox>
-      ) : (
-        <PriceBox>
-          <button
-            type="button"
-            onClick={() => setCurrencyState(!currencyState)}
-          >
-            <img src={currency.flag} />
-            {}
-          </button>
-          {currencyState ? price * exchangeCurrency.bkpr : price}
-          {currencyState ? '원' : exchangeCurrency.cur_nm}
-        </PriceBox>
-      )}
+      <PriceBox>
+        <img src={currency.flag} />
+        {`${currency.code} ${price}`}
+      </PriceBox>
     </Wrapper>
   )
 }
