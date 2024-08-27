f4cd99c63c067a839c8d29768aeb0a521006db75
seoulyego <youngsk786@gmail.com>
2024-08-27 18:59:06 +0900

    KL-147/rename: separate kyInstance to new file
    kyInstance를 별도 파일로 분리하였습니다.
    useKy의 fetch dependency가 추가하였습니다.

--- src/hooks/useKy.js
+++ src/hooks/useKy.js
@@ -1,26 +1,14 @@
-import ky from 'ky'
 import { useState, useEffect } from 'react'
-
-const kyInstance = ky.create({
-  prefixUrl: 'http://localhost:8080/v1/',
-  timeout: 5000,
-  headers: {
-    'content-type': 'application/json',
-  },
-  retry: {
-    limit: 2,
-    methods: ['get', 'post', 'put'],
-    statusCodes: [400, 500],
-  },
-})
+import kyInstance from './kyInstance'
 
 function useKy(initialConfig = null, autoFetch = false, initialData = null) {
-  const [data, setData] = useState(initialData)
   const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(initialData)
   const [error, setError] = useState(null)
 
   const fetchData = async (config) => {
     setLoading(true)
+    setError(null)
     try {
       const response = await kyInstance(config.url, config).json()
       setData(response)
@@ -33,9 +21,9 @@ function useKy(initialConfig = null, autoFetch = false, initialData = null) {
 
   useEffect(() => {
     if (autoFetch && initialConfig) fetchData(initialConfig)
-  }, [])
+  }, [initialConfig])
 
-  return { data, loading, error, fetchData }
+  return { loading, data, error, fetchData }
 }
 
 export default useKy
