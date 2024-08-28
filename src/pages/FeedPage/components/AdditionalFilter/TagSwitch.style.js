129ab8868b3a50da6b23285bde7f950b315f5c97
seoulyego <youngsk786@gmail.com>
2024-08-28 14:26:14 +0900

    KL-147/refactor: fetch tag data based on subcategory query - DataFetcher에서 세팅된 subcategory를 쿼리로 하여 tag 데이터를 요청합니다. - 변수명, 파일명 변경 	- filter -> tag 변경 	- 데이터의 id명(filterId)을 id로 변경

---
+++ src/pages/FeedPage/components/AdditionalFilter/TagSwitch.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import theme from '../../../../styles/theme'
+
+const MessageBox = styled.div`
+  font-size: ${theme.size.textSM};
+  padding: 0.3125rem;
+`
+
+export default MessageBox
