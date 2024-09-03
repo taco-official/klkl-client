1b89d12d4f1d26fa9fb79f62c67f5f28633abf3b
seoulyego <youngsk786@gmail.com>
2024-09-03 16:31:33 +0900

    KL-147/feat: reset states on destroy FeedPage
    FeedPage에서 나가면 모든 selected states를 초기화합니다.

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useFeedStore from '../../stores/useFeedStore'
 import Thumbnail from './components/Thumbnail/Thumbnail'
 import BasicFilter from './components/BasicFilter/BasicFilter'
 import AdditionalFilter from './components/AdditionalFilter/AdditionalFilter'
@@ -8,6 +9,11 @@ import ProductDataStatusRenderer from './components/ProductList/ProductDataStatu
 import { FeedPageLayout, FeedPageContent, FeedArea } from './FeedPage.style'
 
 function FeedPage() {
+  const resetSelectedField = useFeedStore((state) => state.resetSelectedField)
+  useEffect(() => {
+    return resetSelectedField()
+  }, [])
+
   return (
     <FeedPageLayout>
       <Thumbnail />
