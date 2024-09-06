41920010d8a92397498c3a62864592e157030073
seoulyego <youngsk786@gmail.com>
2024-09-06 13:40:55 +0900

    KL-164/design: redesign for Thumbnail of FeedPage
    - Thumbnail의 높이를 변경하였습니다.
    - 이미지 태그를 제거하였습니다. 이제 ThumbnailArea의 배경 이미지로 제공됩니다.

--- src/pages/FeedPage/components/Thumbnail/Thumbnail.jsx
+++ src/pages/FeedPage/components/Thumbnail/Thumbnail.jsx
@@ -3,11 +3,7 @@ import ThumbnailArea from './Thumbnail.style'
 
 function Thumbnail() {
   return (
-    <ThumbnailArea>
-      <img
-        src="https://image.fmkorea.com/files/attach/new3/20231020/3655109/5782408085/6302086124/a35751389c84e91bfa145bce6f81e5ba.jpg"
-        alt="김유연 김나경, 세상에서 가장 얘쁜 애들"
-      />
+    <ThumbnailArea $url="https://image.fmkorea.com/files/attach/new3/20231020/3655109/5782408085/6302086124/a35751389c84e91bfa145bce6f81e5ba.jpg">
       <div>
         <div className="title">기념품 둘러보기</div>
         <div className="description">기념품을 둘러보세요.</div>
