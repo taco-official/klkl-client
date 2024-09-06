50d65e04821a892d04f631ccb4405dcc5e040102
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-09-06 16:48:48 +0900

    KL-159/fix: fix category select feature
    - 바뀐 api에 맞춰 수정

---
+++ src/pages/SubmitPage/components/CategorySubmit/CategorySelectList.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useShallow } from 'zustand/react/shallow'
+import PropTypes from 'prop-types'
+
+import useFormStore from '../../../../stores/useFormStore'
+import SelectionList from '../RegionSubmit/SelectionList'
+
+const useCategorySetter = () => {
+  const setFormContents = useFormStore((state) => state.setFormContents)
+
+  const setCategory = (category) =>
+    setFormContents({ category, subcategory: undefined })
+
+  const setSubcategory = (subcategory) => setFormContents({ subcategory })
+
+  return [setCategory, setSubcategory]
+}
+
+function CategorySelectList({ categories }) {
+  const [category, subcategory] = useFormStore(
+    useShallow((state) => [state.category, state.subcategory])
+  )
+  const [setCategory, setSubcategory] = useCategorySetter()
+
+  return (
+    <ListWrapper>
+      <SelectionList
+        optionList={categories}
+        optionState={category?.id}
+        setOptionState={setCategory}
+        $width={`${100 / 2}%`}
+      />
+      {category && (
+        <SelectionList
+          optionList={category.subcategories}
+          optionState={subcategory?.id}
+          setOptionState={setSubcategory}
+          $width={`${100 / 2}%`}
+        />
+      )}
+    </ListWrapper>
+  )
+}
+CategorySelectList.propTypes = {
+  categories: PropTypes.arrayOf({
+    id: PropTypes.number,
+    name: PropTypes.string,
+  }).isRequired,
+}
+
+const ListWrapper = styled.div`
+  width: 37.5rem;
+  display: flex;
+  margin-bottom: 1.875rem;
+`
+
+export default CategorySelectList
