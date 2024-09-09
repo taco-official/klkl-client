2885a951ec3cb58e39c5b32e90e4da4387fd629d
seoulyego <youngsk786@gmail.com>
2024-09-09 15:51:40 +0900

    KL-162/feat: use location state
    FeedPage의 location state를 확인합니다.

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import useFeedStore from '../../stores/useFeedStore'
 import Thumbnail from './components/Thumbnail/Thumbnail'
 import BasicFilter from './components/BasicFilter/BasicFilter'
@@ -10,9 +11,13 @@ import { FeedPageLayout, FeedPageContent, FeedArea } from './FeedPage.style'
 
 function FeedPage() {
   const resetSelectedField = useFeedStore((state) => state.resetSelectedField)
+  const location = useLocation()
+
   useEffect(() => {
     return resetSelectedField()
   }, [])
+
+  console.log(location.state)
 
   return (
     <FeedPageLayout>
