25bf24a22038b11875081aa5fc0263234429f5a2
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-10-01 18:11:20 +0900

    KL-159/chore: change vite.config for import directory
    - import 경로를 상대경로에서 절대경로로 바꾸기 위해 vite.config.mjs 수정
    - eslint-import-resolver-alias, eslint-module-utils, eslint-plugin-import 버전 업데이트
    - 절대 경로로 바꾸면서 생기는 에러를 해결하기 위해 eslint 설정 변경
    - yarn version 최신으로 변경

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@constants',
+        replacement: path.resolve(__dirname, 'src/constants'),
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '@images',
+        replacement: path.resolve(__dirname, 'src/images'),
+      },
+      {
+        find: '@loader',
+        replacement: path.resolve(__dirname, 'src/loader'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@stores',
+        replacement: path.resolve(__dirname, 'src/stores'),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, 'src/styles'),
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
+    ],
+  },
 })
