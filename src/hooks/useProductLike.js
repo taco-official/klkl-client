e2f1be0e918bacc557496fd86b26fd9b143bae13
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-10-04 18:51:00 +0900

    KL-159/refactor: unify error handling

--- src/hooks/useProductLike.js
+++ src/hooks/useProductLike.js
@@ -2,33 +2,16 @@ import { method } from './kyInstance'
 import useKyMutation from './useKyMutation'
 
 const useProductLike = (productId) => {
-  const { mutateAsync: like } = useKyMutation(
+  const { mutateAsync: likeProduct } = useKyMutation(
     method.POST,
-    `products/${productId}/likes`,
-    ['products/likes', productId]
+    `likes/${productId}`,
+    ['likes', productId]
   )
-  const { mutateAsync: unlike } = useKyMutation(
+  const { mutateAsync: unlikeProduct } = useKyMutation(
     method.DELETE,
-    `products/${productId}/likes`,
-    ['products/likes', productId]
+    `likes/${productId}`,
+    ['likes', productId]
   )
-
-  const likeProduct = async () => {
-    try {
-      await like()
-    } catch (error) {
-      console.error(error)
-      // alert('문제가 발생했습니다. 잠시 후 다시 시도해주세요.')
-    }
-  }
-  const unlikeProduct = async () => {
-    try {
-      await unlike()
-    } catch (error) {
-      console.error(error)
-      // alert('문제가 발생했습니다. 잠시 후 다시 시도해주세요.')
-    }
-  }
 
   return { likeProduct, unlikeProduct }
 }
