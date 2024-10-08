a4b44a55bf903c340927ef4080925ac3dca11755
sayoon-mandarine <sayoon@student.42seoul.kr>
2024-10-08 15:50:21 +0900

    KL-159/chore: optimize build
    - 빌드 최적화
    : 여러군데서 사용하는 라이브러리를 하나의 파일로 분리하여 용량을 축소시켰습니다

--- vite.config.mjs
+++ vite.config.mjs
@@ -48,4 +48,16 @@ export default defineConfig({
       },
     ],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.indexOf('node_modules') !== -1) {
+            const module = id.split('node_modules/').pop().split('/')[0]
+            return `vendor/${module}`
+          }
+        },
+      },
+    },
+  },
 })
