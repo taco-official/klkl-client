fcd0c393f4f2f65d398f8349505a0f453b6a3e2e
seoulyego <youngsk786@gmail.com>
2024-10-08 18:23:39 +0900

    KL-185/fix: fix logic for logout
    - 로그아웃 로직을 수정하였습니다.

--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -1,11 +1,32 @@
+import { useEffect } from 'react'
+import { isEqual } from 'lodash-es'
+import useLoginStore from '@stores/useLoginStore'
 import useKyQuery from './useKyQuery'
 
 const useUserData = () => {
-  return useKyQuery('me', undefined, {
+  const { isLoading, data, isError } = useKyQuery('me', undefined, {
     staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 60,
-    select: (data) => data.data,
+    select: (userData) => userData.data,
   })
+  const { isLogin, loginData } = useLoginStore((state) => ({
+    isLogin: state.isLogin,
+    loginData: state.loginData,
+  }))
+  const { setLogin, setLoginData, setLogout } = useLoginStore((state) => ({
+    setLogin: state.setLogin,
+    setLoginData: state.setLoginData,
+    setLogout: state.setLogout,
+  }))
+
+  useEffect(() => {
+    if (isLoading) return
+    if (data) {
+      if (!isLogin) setLogin(data)
+      else if (!isEqual(loginData, data)) setLoginData(data)
+    }
+    if (isError && isLogin) setLogout()
+  }, [isLoading, data, isError])
 }
 
 export default useUserData
